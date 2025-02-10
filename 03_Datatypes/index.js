578c1ed114c61644fa4c6f40085ba85b8a0ada29
ishu-rajput04 <official.ishurajput04@gmail.com>
2025-02-10 23:44:32 +0530

    Add files via upload

---
+++ 03_Datatypes/index.js
@@ -0,0 +1,16 @@
+
+// ------------here we see the different type of primitive data types----------
+
+// let marks = 90;  //  number
+// marks =" ishu "; //  string
+// marks=true;      //  boolean
+
+let marks;          // undefined
+
+marks=null;         // null
+console.log(marks);
+
+
+/// if we wnt to know which type of data varible have then we do this;
+marks=390;
+console.log(typeof(marks));
