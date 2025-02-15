2fb0796a3631c1987818245ba16da34d6e9530bc
ishu-rajput04 <official.ishurajput04@gmail.com>
2025-02-15 14:45:07 +0530

    Add files via upload

---
+++ 05_Conditionals/index.js
@@ -0,0 +1,37 @@
+
+//------if-else------
+
+let age = 19;
+if (age >= 18) {
+    console.log("i vote");
+}
+else {
+    console.log("i can not vote");
+};
+
+// else-if else-if------------
+
+let marks = 45;
+if (marks == 45) {
+    console.log("pass");
+
+} else {
+    console.log("fail");
+}
+
+
+/// switch
+
+let num = 3;
+switch (num) {
+    case 1: console.log("a");
+        break;
+    case 2: console.log("b");
+        break;
+    case 3: console.log("c");
+        break;
+    case 4: console.log("d");
+        break;
+    default: console.log("f");
+
+}
