c28024d42866d71f89a61a7c9b8c39450e8f7637
ishu-rajput04 <official.ishurajput04@gmail.com>
2025-04-06 19:20:20 +0530

    Add files via upload

---
+++ 10_Hoisting/index.js
@@ -0,0 +1,11 @@
+//--------------------------variable hoisting-----------------
+
+console.log(age);
+var age=21;
+
+//--------------------------Function Hoisting-----------------
+
+saymyname("ishu rajput");
+function saymyname(fullname){
+    console.log(fullname);
+}
